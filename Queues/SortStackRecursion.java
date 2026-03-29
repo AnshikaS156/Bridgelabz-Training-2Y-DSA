3a1e6a1e1d312e29ddf24a47411887e83da134c4
AnshikaS156 <saraswat.anshika156@gmail.com>
2026-03-29 22:33:45 +0530

    Queue

---
+++ Queues/SortStackRecursion.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class SortStackRecursion {
+
+    static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+            sortStack(stack);
+            insertSorted(stack, temp);
+        }
+    }
+
+    static void insertSorted(Stack<Integer> stack, int value) {
+        if (stack.isEmpty() || stack.peek() <= value) {
+            stack.push(value);
+            return;
+        }
+
+        int temp = stack.pop();
+        insertSorted(stack, value);
+        stack.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+
+        sortStack(stack);
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
